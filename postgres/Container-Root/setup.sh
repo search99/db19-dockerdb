bc2a38d1f14827b6aa355b7fc1c698b0467cd627
guom <guom@zucc.edu.cn>
2019-07-30 22:42:48 +0800

    add: setup.sh

--- postgres/Container-Root/setup.sh
+++ postgres/Container-Root/setup.sh
@@ -5,5 +5,11 @@ if [ -d /etc/apt ]; then
         [ -n "$https_proxy" ] && echo "Acquire::https::proxy \"${https_proxy}\";" >> /etc/apt/apt.conf; \
         [ -f /etc/apt/apt.conf ] && cat /etc/apt/apt.conf
 fi
+apt update -yqq
+apt install python3 -yqq
+apt install python3-pip -yqq
+apt install postgresql-server-dev-11 -yqq
+pip3 install pgcli six
 
-
+apt remove postgresql-server-dev-11 -yqq
+apt autoremove -yqq
